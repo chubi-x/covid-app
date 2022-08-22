e3dab702af78d46bfb144c14133209983f79149b
chubi-x <chubixax@protonmail.com>
2022-08-22 15:18:24 +0100

    feat: update cases component to use api data

--- src/Components/TotalStats.js
+++ src/Components/TotalStats.js
@@ -1,19 +1,41 @@
 import React from "react";
 import Cases from "./totals/Cases";
-import corona from "../images/corona.svg";
 import bed from "../images/bed.svg";
 import dead from "../images/dead.svg";
 import heart from "../images/heart.svg";
 import mask from "../images/mask.svg";
 
+import { CasesContext } from "../Helper/Context";
+import { useContext } from "react";
 export default function TotalStats() {
+  const { data } = useContext(CasesContext);
   return (
-    <div className="container flex justify-between px-24 pt-10">
-      <Cases title="Total Samples" number={0} icon={heart} />
-      <Cases title=" Confirmed Cases" number={0} icon={mask} />
-      <Cases title="Total Active Cases" number={0} icon={bed} />
-      <Cases title="Total Discharged" number={0} icon={heart} />
-      <Cases title="Total Deaths" number={0} icon={dead} />
-    </div>
+    <>
+      {data.map((data, index) => (
+        <div key={index} className="container flex justify-between px-32 pt-10">
+          <Cases
+            title="Total Samples"
+            number={data.totalSamplesTested}
+            icon={heart}
+          />
+          <Cases
+            title=" Confirmed Cases"
+            number={data.totalConfirmedCases}
+            icon={mask}
+          />
+          <Cases
+            title="Total Active Cases"
+            number={data.totalActiveCases}
+            icon={bed}
+          />
+          <Cases
+            title="Total Discharged"
+            number={data.discharged}
+            icon={heart}
+          />
+          <Cases title="Total Deaths" number={data.death} icon={dead} />
+        </div>
+      ))}
+    </>
   );
 }
