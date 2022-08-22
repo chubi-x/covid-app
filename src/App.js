23db014c4caf16f711dee40bcb0ca5000d204699
chubi-x <chubixax@protonmail.com>
2022-08-22 19:01:31 +0100

    feat: implement loading state

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import TotalStats from "./Components/totals/TotalStats";
 import States from "./Components/states/States";
 import { CasesContext } from "./Helper/Context";
 function App() {
+  const [loading, setLoading] = useState(true);
   const [data, setData] = useState([]);
   const [states, setStates] = useState([]);
 
@@ -26,21 +27,39 @@ function App() {
           console.log(err.name);
         }
       });
+
     return () => {
       controller.abort();
     };
   }, []);
 
+  setTimeout(() => {
+    setLoading(false);
+  }, 3000);
+
   return (
     <CasesContext.Provider value={{ data, states }}>
-      <div className="App">
-        <Header />
-        <div className="px-20 md:px-32 pt-10">
-          <TotalStats />
-          <div className="banner"></div>
-          <States />
+      {loading ? (
+        <div className="loading-state flex justify-center pt-32">
+          <lottie-player
+            src="https://assets7.lottiefiles.com/packages/lf20_v1c99zh5.json"
+            background="transparent"
+            speed="1"
+            style={{ width: "300px", height: "300px" }}
+            loop
+            autoplay
+          ></lottie-player>
         </div>
-      </div>
+      ) : (
+        <div className="App">
+          <Header />
+          <div className="px-20 md:px-32 pt-10">
+            <TotalStats />
+            <div className="banner"></div>
+            <States />
+          </div>
+        </div>
+      )}
     </CasesContext.Provider>
   );
 }
